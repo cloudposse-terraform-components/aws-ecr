6036e3f658625eef1c6b5b44839e8fa1b7d71cba
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-20 15:45:49 +0300

    Update module sources and add ECR registry policy

--- src/main.tf
+++ src/main.tf
@@ -3,17 +3,31 @@ locals {
 }
 
 module "full_access" {
-  source = "../account-map/modules/roles-to-principals"
+  source = "github.com/cloudposse-terraform-components/aws-account-map//src/modules/roles-to-principals?ref=v1.536.1"
 
   role_map = var.read_write_account_role_map
+
+  tenant      = var.account_map_enabled ? module.iam_roles.global_tenant_name : null
+  environment = var.account_map_enabled ? module.iam_roles.global_environment_name : null
+  stage       = var.account_map_enabled ? module.iam_roles.global_stage_name : null
+
+  account_map_bypass   = !var.account_map_enabled
+  account_map_defaults = var.account_map
 
   context = module.this.context
 }
 
 module "readonly_access" {
-  source = "../account-map/modules/roles-to-principals"
+  source = "github.com/cloudposse-terraform-components/aws-account-map//src/modules/roles-to-principals?ref=v1.536.1"
 
   role_map = var.read_only_account_role_map
+
+  tenant      = var.account_map_enabled ? module.iam_roles.global_tenant_name : null
+  environment = var.account_map_enabled ? module.iam_roles.global_environment_name : null
+  stage       = var.account_map_enabled ? module.iam_roles.global_stage_name : null
+
+  account_map_bypass   = !var.account_map_enabled
+  account_map_defaults = var.account_map
 
   context = module.this.context
 }
@@ -62,4 +76,30 @@ resource "aws_ecr_pull_through_cache_rule" "this" {
   ecr_repository_prefix = each.key
   upstream_registry_url = each.value.registry
   credential_arn        = length(each.value.secret) > 0 ? data.aws_secretsmanager_secret.cache_credentials[each.key].arn : null
+}
+
+data "aws_caller_identity" "current" {
+  count = local.enabled ? 1 : 0
+}
+
+resource "aws_ecr_registry_policy" "this" {
+  for_each = toset(local.enabled &&  length(var.pull_through_cache_rules) > 0 ? ["true"] : [])
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetImageCopyStatus",
+          "ecr:BatchImportUpstreamImage"
+        ]
+        Principal = {
+          AWS = distinct(compact(concat(module.full_access.principals, module.readonly_access.principals, [local.ecr_user_arn])))
+        }
+        Resource = format("arn:aws:ecr:%s:%s:repository/*", var.region, one(data.aws_caller_identity.current.*.account_id))
+      }
+    ]
+  })
 }
